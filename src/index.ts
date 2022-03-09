08ae4afd0740b106da6d4ff469b35e1d8af1ffe0
Charles Fisher <charles.fisher@americaneagle.com>
2022-03-09 13:51:36 -0600

    add set method to user class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello world')
+import { User } from "./models/User";
+
+const user = new User({name: 'Raven', age: 9 });
+
+user.set({name: 'Olivia', age: 10})
+console.log(user.get('name'));
+console.log(user.get('age'));
+
+user.set({name: "Cha cha"})
+console.log(user.get('name'));
+
