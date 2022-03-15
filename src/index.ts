c956021239292ddb50daf621d0dd95bb227f0d6e
Charles Fisher <charles.fisher@americaneagle.com>
2022-03-15 13:21:52 -0500

    set up json server

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,8 @@ console.log(user.get('age'));
 user.set({name: "Cha cha"})
 console.log(user.get('name'));
 
+user.on('change', () => {console.log('Change #1')});
+user.on('state', () => {console.log('Change #2')});
+user.on('update', () => {console.log('Change #3')});
+
+user.trigger('update')
--- src/models/User.ts
+++ src/models/User.ts
@@ -3,10 +3,10 @@ interface UserProps {
     age?: number
 }
 
-type Callback = () => {};
+type Callback = () => void;
 
 export class User {
-    
+    events: { [key: string]: Callback[] } = {} //This tells TS that our events object will take a key that's a string and an array of callback functions
 
     constructor(private data: UserProps){};
 
@@ -18,7 +18,19 @@ export class User {
         Object.assign(this.data, update);
     }
 
-    on(eventName: string, callback: Callback) {
+    on(eventName: string, callback: Callback): void {
+        const handlers = this.events[eventName] || [];
+        handlers.push(callback);
+        this.events[eventName] = handlers;
+    }
 
+    trigger(eventName: string): void {
+        const handlers = this.events[eventName];
+
+        if(!handlers || handlers.length === 0) {
+            return;
+        }
+
+        handlers.forEach(callback => callback());
     }
 }
